e1873c874c5bf24f680cd5417fedef6fc1aea51e
BrunnoAlves0011 <brunno3dgamer@gmail.com>
2024-09-18 12:12:12 -0300

    v0.01

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(DevicePreview(enabled: true, builder: (context) => MainApp()));
 }
 
 class MainApp extends StatelessWidget {
@@ -10,6 +13,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
